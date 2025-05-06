a93deac45d56410dbb27459d7eb80213dfc1114d
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-05-06 18:37:34 +0300

    Взял за основу шаблон, добавил возможность отображения карточек музейных событий.

---
+++ Museum.Frontend/Components/Pages/Home/Components/MuseumEventListComponent.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Museum.Frontend.Services;
+
+namespace Museum.Frontend.Components.Pages.Home.Components
+{
+    public class MuseumEventListComponentModel : ComponentBase
+    {
+        [Inject]
+        public MuseumClient Api {  get; set; }
+
+        public string BaseUrl => Api.BaseUrl;
+
+        [Parameter]
+        public MuseumEventListVm MuseumEventList { get; set; }
+    }
+}
