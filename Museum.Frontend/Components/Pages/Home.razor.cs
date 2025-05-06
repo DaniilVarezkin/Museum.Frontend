0e3ff5e8f1eff6de849bf09177ca25546262346b
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-05-06 16:13:42 +0300

    Add project files.

---
+++ Museum.Frontend/Components/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Museum.Frontend.Services;
+
+namespace Museum.Frontend.Components.Pages
+{
+    public class HomeViewModel : ComponentBase
+    {
+        [Inject] public MuseumClient api {  get; set; }
+
+        public MuseumEventListVm? response = null;
+        public string BaseUrl => api.BaseUrl;
+
+        public async Task GetAllEventsAsync()
+        {
+            response = await api.GetAllAsync();
+        }
+    }
+}
---
+++ Museum.Frontend/Program.cs
@@ -0,0 +1,37 @@
+using Museum.Frontend.Components;
+using Museum.Frontend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+
+var baseUrl = builder.Configuration["BaseUrl"];
+
+builder.Services.AddTransient(typeof(MuseumClient), (provider) =>
+{
+    return new MuseumClient(baseUrl, provider.GetRequiredService<HttpClient>());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
